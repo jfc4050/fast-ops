bf170ba682ab054cbce5a3b8f700efe19a8c1a44
Justin Chiu <jfc4050@gmail.com>
2023-03-11 22:11:37 +0000

    fix test

--- tests/test_flash_attention.py
+++ tests/test_flash_attention.py
@@ -6,11 +6,12 @@ def test_flash_attn():
     batch_sz = 1
     n_heads = 1
     seq_len = 16
+    head_dim = 32
     dtype = torch.bfloat16
 
-    Q = torch.randn(batch_sz, n_heads, seq_len, seq_len, dtype=dtype)
-    K = torch.randn(batch_sz, n_heads, seq_len, seq_len, dtype=dtype)
-    V = torch.randn(batch_sz, n_heads, seq_len, seq_len, dtype=dtype)
+    Q = torch.randn(batch_sz, n_heads, seq_len, head_dim, dtype=dtype)
+    K = torch.randn(batch_sz, n_heads, seq_len, head_dim, dtype=dtype)
+    V = torch.randn(batch_sz, n_heads, seq_len, head_dim, dtype=dtype)
     out = FlashAttentionFunction.apply(Q, K, V)
 
     print(out)
