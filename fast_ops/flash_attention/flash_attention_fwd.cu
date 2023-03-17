a025ce384c594145096ca2b8aef45ca1cda50154
Justin Chiu <jfc4050@gmail.com>
2023-03-17 01:30:30 +0000

    loading Kj to SRAM

--- fast_ops/flash_attention/flash_attention_fwd.cu
+++ fast_ops/flash_attention/flash_attention_fwd.cu
@@ -12,6 +12,16 @@
 #include "cute/stride.hpp"
 #include "cute/swizzle_layout.hpp"
 
+template <typename scalar_t, int BLOCK_M, int BLOCK_N, int BLOCK_D>
+struct flash_attn_fwd_smem {
+  scalar_t Qi[BLOCK_M * BLOCK_D];
+
+  union {
+    scalar_t Kj[BLOCK_N * BLOCK_D];
+    scalar_t Vj[BLOCK_N * BLOCK_D];
+  };
+};
+
 template <typename scalar_t_pt, int BLOCK_M, int BLOCK_N, int BLOCK_D>
 __global__ void flash_attn_fwd_kernel(
     const at::PackedTensorAccessor32<scalar_t_pt, 4, at::RestrictPtrTraits>
@@ -26,7 +36,7 @@ __global__ void flash_attn_fwd_kernel(
   // map PyTorch type to CUTLASS type
   using scalar_t = typename cutlass_t<scalar_t_pt>::value;
 
-  __shared__ scalar_t Qi_smem[BLOCK_M * BLOCK_D];
+  __shared__ flash_attn_fwd_smem<scalar_t, BLOCK_M, BLOCK_N, BLOCK_D> smem;
 
   const int seqlen_m = Q_accessor.size(3); // number of queries
   const int seqlen_n = K_accessor.size(3); // number of keys/values
@@ -47,13 +57,18 @@ __global__ void flash_attn_fwd_kernel(
                         cute::make_layout(cute::make_shape(Q_accessor.size(3),
                                                            Q_accessor.size(4)),
                                           cute::GenRowMajor{}));
-  // TODO. do K and V as well
+  auto K =
+      cute::make_tensor(cute::make_gmem_ptr(reinterpret_cast<scalar_t *>(
+                            K_accessor[batch_idx][head_idx].data())),
+                        cute::make_layout(cute::make_shape(K_accessor.size(3),
+                                                           K_accessor.size(4)),
+                                          cute::GenRowMajor{}));
+  // TODO. do V as well
 
-  // represent SRAM tiles
+  // load Qi into SRAM - this is loop invariant and is only loaded once.
   auto Qi =
-      cute::make_tensor(cute::make_smem_ptr(Qi_smem),
+      cute::make_tensor(cute::make_smem_ptr(smem.Qi),
                         cute::make_layout(cute::make_shape(BLOCK_M, BLOCK_D)));
-  // make tile to be copied into SRAM
   auto Qi_gmem_tile = cute::local_tile(Q, cute::make_shape(BLOCK_M, BLOCK_D),
                                        cute::make_coord(start_m, 0));
   auto Qi_load_thread_layout =
@@ -66,7 +81,20 @@ __global__ void flash_attn_fwd_kernel(
 
   for (int seq_chunk_n_start = 0; seq_chunk_n_start < seqlen_n;
        seq_chunk_n_start += BLOCK_N) {
-    ;
+
+    // load Kj into SRAM
+    auto Kj = cute::make_tensor(
+        cute::make_smem_ptr(smem.Kj),
+        cute::make_layout(cute::make_shape(BLOCK_N, BLOCK_D)));
+    auto Kj_gmem_tile = cute::local_tile(Kj, cute::make_shape(BLOCK_N, BLOCK_D),
+                                         cute::make_coord(start_m, 0));
+    auto Kj_load_thread_layout =
+        cute::make_shape(cute::Int<32>{}, cute::Int<8>{});
+    auto Kj_load_partition_gmem =
+        cute::local_partition(Kj_gmem_tile, Kj_load_thread_layout, thread_id);
+    auto Kj_load_partition_smem =
+        cute::local_partition(Kj, Kj_load_thread_layout, thread_id);
+    cute::copy(Kj_load_partition_gmem, Kj_load_partition_smem);
   }
 }
 
@@ -89,7 +117,7 @@ at::Tensor flash_attn_fwd_cuda(at::Tensor Q, at::Tensor K, at::Tensor V) {
       Q.scalar_type(), "flash_attn_fwd", ([&] {
         // TODO. double check block dim
         flash_attn_fwd_kernel<scalar_t, BLOCK_M, BLOCK_N, BLOCK_D>
-            <<<dim3(batch_sz, n_heads, n_blocks_m), dim3(BLOCK_M, BLOCK_N)>>>(
+            <<<dim3(batch_sz, n_heads, n_blocks_m), dim3(8, 32)>>>(
                 Q.packed_accessor32<scalar_t, 4, at::RestrictPtrTraits>(),
                 K.packed_accessor32<scalar_t, 4, at::RestrictPtrTraits>(),
                 V.packed_accessor32<scalar_t, 4, at::RestrictPtrTraits>(),
