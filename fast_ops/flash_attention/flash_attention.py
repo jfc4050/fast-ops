578dbba0ade950de27615dd6ba696743129383f9
Justin Chiu <jfc4050@gmail.com>
2023-03-17 01:50:38 +0000

    docstrings for python function

--- fast_ops/flash_attention/flash_attention.py
+++ fast_ops/flash_attention/flash_attention.py
@@ -20,8 +20,38 @@ flash_attention_ext = load(
 
 
 class FlashAttentionFunction(Function):
+    """
+    See FlashAttention paper:
+    FlashAttention: Fast and Memory-Efficient Exact Attention with IO-Awareness
+    https://arxiv.org/pdf/2205.14135.pdf
+
+    for shape documentation we use the following notation:
+    - B: batch size
+    - H: number of heads
+    - M: number of queries (sequence length)
+    - N: number of keys (sequence length)
+    - D: head dim
+
+    Limitations:
+    Violating any of these constraints will result in an exception.
+    * `head_dim` must be <= 128. Otherwise tiles become too large to keep in SRAM.
+      If you'd like to use larger values of `head_dim`, consider using the
+      xFormers implementation (https://github.com/facebookresearch/xformers).
+    * `head_dim` must be a multiple of 8 - this is an alignment requirement to support
+      128b vectorized loads.
+    * The last two dimensions of `Q`, `K`, `V`, and `attn_bias` must be contiguous and
+      row-major 2D matrix
+    * Inputs must be half-precision floating points (float16 and bfloat16)
+    """
+
     @staticmethod
     def forward(ctx, Q: Tensor, K: Tensor, V: Tensor) -> Tensor:
+        """
+        :param Q: (B, H, M, D) Query tensor.
+        :param K: (B, H, N, D) Key tensor.
+        :param V: (B, H, N, D) Value tensor.
+        :returns: (B, H, M, D)
+        """
         # TODO. handle dynamic dispatch here
         return flash_attention_ext.forward(Q, K, V)
 
