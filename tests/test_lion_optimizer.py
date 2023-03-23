d1a1dfe6adc9687eaffedf26c45e66abe17b6ef3
Justin Chiu <jfc4050@gmail.com>
2023-03-23 17:38:27 +0000

    lion test updates

--- tests/test_lion_optimizer.py
+++ tests/test_lion_optimizer.py
@@ -6,13 +6,13 @@ from fast_ops.lion.lion_optimizer_ref import Lion as LionRef
 
 
 @pytest.mark.parametrize("weight_decay", [0.0, 0.1])
-@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16, torch.float32], ids=str)
+@pytest.mark.parametrize("dtype", [torch.bfloat16], ids=str)
 def test_lion(weight_decay: float, dtype: torch.dtype) -> None:
     lr = 1e-4
     betas = (0.9, 0.99)
 
     master_params = [
-        torch.rand(100, requires_grad=True, dtype=dtype, device="cuda") for _ in range(3)
+        torch.rand(256, requires_grad=True, dtype=dtype, device="cuda") for _ in range(3)
     ]
     params = [p.clone().detach() for p in master_params]
     params_ref = [p.clone().detach() for p in master_params]
@@ -20,7 +20,7 @@ def test_lion(weight_decay: float, dtype: torch.dtype) -> None:
     lion = Lion(params, lr=lr, betas=betas, weight_decay=weight_decay)
     lion_ref = LionRef(params_ref, lr=lr, betas=betas, weight_decay=weight_decay)
 
-    for _ in range(5):  # simulate a few training steps
+    for iter in range(5):  # simulate a few training steps
         for param, param_ref in zip(params, params_ref):
             grad = torch.randn_like(param, requires_grad=False)
             param.grad = grad
@@ -30,7 +30,7 @@ def test_lion(weight_decay: float, dtype: torch.dtype) -> None:
         lion_ref.step()
 
         for param, param_ref in zip(params, params_ref):
-            assert torch.allclose(param, param_ref, rtol=1e-4, atol=1e-4)
+            assert torch.allclose(param, param_ref, rtol=1e-4, atol=1e-4), f"param mismatch on iter {iter}"
             exp_avg = lion.state[param]["exp_avg"]
             exp_avg_ref = lion_ref.state[param_ref]["exp_avg"]
-            assert torch.allclose(exp_avg, exp_avg_ref, rtol=1e-4, atol=1e-4)
+            assert torch.allclose(exp_avg, exp_avg_ref, rtol=1e-4, atol=1e-4), f"exp_avg mismatch on iter {iter}"
