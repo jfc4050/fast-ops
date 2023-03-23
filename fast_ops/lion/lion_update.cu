a7cdaf71b1d5f991672ddb2ec796ce38a595bac9
Justin Chiu <jfc4050@gmail.com>
2023-03-23 02:23:32 +0000

    poc kernel code done

--- fast_ops/lion/lion_update.cu
+++ fast_ops/lion/lion_update.cu
@@ -20,22 +20,25 @@ __global__ void lion_update_kernel(
     const float weight_decay) {
 
   // 128bit/16byte access per thread
+  static_assert(sizeof(scalar_t) == sizeof(momentum_t), "restriction for now");
   constexpr int ACCESS_N = 16 / sizeof(scalar_t);
   using AccessType = scalar_t[ACCESS_N];
+  using MomentumAccessType = momentum_t[ACCESS_N];
 
   AccessType *param_accesses = reinterpret_cast<AccessType *>(param);
   const AccessType *grad_accesses = reinterpret_cast<AccessType *>(grad);
-  AccessType *momentum_accesses = reinterpret_cast<AccessType *>(grad);
+  MomentumAccessType *momentum_accesses = reinterpret_cast<AccessType *>(grad);
 
   const scalar_t weight_decay_factor = 1.0 - lr * weight_decay;
   const scalar_t beta1_complement = 1.0 - beta1;
+  const scalar_t beta2_complement = 1.0 - beta2;
   const scalar_t neg_lr = -lr;
-  for (int thread_start = blockIdx.x * blockDim.x + threadIdx.x * ACCESS_N;
-       thread_start < numel;
-       thread_start += blockDim.x) {
-    AccessType param_access = param_accesses[thread_start];
-    AccessType grad_access = grad_accesses[thread_start];
-    AccessType momentum_access = momentum_accesses[thread_start];
+  for (int thread_iter_idx = blockIdx.x * blockDim.x + threadIdx.x * ACCESS_N;
+       thread_iter_idx < numel;
+       thread_iter_idx += blockDim.x) {
+    AccessType param_access = param_accesses[thread_iter_idx];
+    AccessType grad_access = grad_accesses[thread_iter_idx];
+    MomentumAccessType momentum_access = momentum_accesses[thread_iter_idx];
 
     // apply weight decay
     // TODO. make sure this vectorizes
@@ -66,12 +69,19 @@ __global__ void lion_update_kernel(
     }
 
     // write back
-    *param_accesses[thread_start] = param_access;
+    *param_accesses[thread_iter_idx] = param_access;
 
     // decay momentum
     for (int i = 0; i < ACCESS_N; ++i) {
-      ;
+      momentum_accesses[i] *= beta2;
     }
+    for (int i = 0; i < ACCESS_N; ++i) {
+      momentum_access[i] =
+          grad_access[i] * beta2_complement + momentum_access[i];
+    }
+
+    // write back momentum
+    *momentum_accesses[thread_iter_idx] = momentum_access;
   }
 }
 
