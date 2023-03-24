e261c237cfab210a4636559558cfa7be11924cfd
Justin Chiu <jfc4050@gmail.com>
2023-03-24 00:35:04 +0000

    update test

--- tests/test_lion_optimizer.py
+++ tests/test_lion_optimizer.py
@@ -4,15 +4,16 @@ import torch
 from fast_ops.lion.lion_optimizer import Lion
 from fast_ops.lion.lion_optimizer_ref import Lion as LionRef
 
+torch.set_printoptions(sci_mode=False)
 
-@pytest.mark.parametrize("weight_decay", [0.5])
-@pytest.mark.parametrize("dtype", [torch.bfloat16], ids=str)
+@pytest.mark.parametrize("weight_decay", [0.0, 0.5])
+@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16], ids=str)
 def test_lion(weight_decay: float, dtype: torch.dtype) -> None:
     lr = 1e-2
     betas = (0.9, 0.99)
 
     master_params = [
-        torch.rand(256, requires_grad=True, dtype=dtype, device="cuda") for _ in range(1)
+        torch.rand(32, requires_grad=True, dtype=dtype, device="cuda") for _ in range(3)
     ]
     params = [p.clone().detach() for p in master_params]
     params_ref = [p.clone().detach() for p in master_params]
@@ -29,12 +30,16 @@ def test_lion(weight_decay: float, dtype: torch.dtype) -> None:
         lion.step()
         lion_ref.step()
 
-        for param, param_ref in zip(params, params_ref):
+        for param_idx, (param, param_ref) in enumerate(zip(params, params_ref)):
+            # print(param)
+            # print(param_ref)
             assert torch.allclose(
-                param, param_ref, rtol=1e-4, atol=1e-4
-            ), f"param mismatch on iter {iter}"
+                param, param_ref, rtol=1e-3, atol=1e-3
+            ), f"param mismatch on param {param_idx}, iter {iter}"
             exp_avg = lion.state[param]["exp_avg"]
             exp_avg_ref = lion_ref.state[param_ref]["exp_avg"]
+            # print(exp_avg)
+            # print(exp_avg_ref)
             assert torch.allclose(
-                exp_avg, exp_avg_ref, rtol=1e-4, atol=1e-4
-            ), f"exp_avg mismatch on iter {iter}"
+                exp_avg, exp_avg_ref, rtol=1e-3, atol=1e-3
+            ), f"exp_avg mismatch on param {param_idx}, iter {iter}"
