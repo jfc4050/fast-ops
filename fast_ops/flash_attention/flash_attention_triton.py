2c2474f0e3feccecd287786f78091e5101fab6ab
Justin Chiu <jfc4050@gmail.com>
2023-03-27 18:43:28 +0000

    remove some dead code

--- fast_ops/flash_attention/flash_attention_triton.py
+++ fast_ops/flash_attention/flash_attention_triton.py
@@ -97,9 +97,6 @@ def _fwd_kernel(
     off_hb = tl.program_id(1)
     off_b = off_hb // nheads
     off_h = off_hb % nheads
-    # off_b = tl.program_id(1)
-    # off_h = tl.program_id(2)
-    # off_hb = off_b * nheads + off_h
     # initialize offsets
     offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
     offs_d = tl.arange(0, BLOCK_HEADDIM)
@@ -307,267 +304,6 @@ def _bwd_preprocess_do_o_dot(
     delta = tl.sum(o * do, axis=1)
     # write-back
     tl.store(Delta + off_hb * seqlen_q_rounded + offs_m, delta)
-
-
-@triton.jit
-def _bwd_kernel_one_col_block(
-    start_n,
-    Q,
-    K,
-    V,
-    Bias,
-    DO,
-    DQ,
-    DK,
-    DV,
-    LSE,
-    D,
-    softmax_scale,
-    dropout_p,
-    rng_seed,
-    dropout_rng_offset_hb,
-    stride_qm,
-    stride_kn,
-    stride_vn,
-    stride_bm,
-    stride_dom,
-    stride_dqm,
-    stride_dkn,
-    stride_dvn,
-    seqlen_q,
-    seqlen_k,
-    headdim,
-    USE_DROPOUT: tl.constexpr,
-    BIAS_TYPE: tl.constexpr,
-    IS_CAUSAL: tl.constexpr,
-    BLOCK_HEADDIM: tl.constexpr,
-    EVEN_M: tl.constexpr,
-    EVEN_N: tl.constexpr,
-    EVEN_HEADDIM: tl.constexpr,
-    BLOCK_M: tl.constexpr,
-    BLOCK_N: tl.constexpr,
-):
-    # initialize row/col offsets
-    offs_n = start_n * BLOCK_N + tl.arange(0, BLOCK_N)
-    offs_d = tl.arange(0, BLOCK_HEADDIM)
-
-    # load bias if loop-invariant
-    if BIAS_TYPE == "vector":
-        # vector bias is same over all values of m, so
-        # can be loaded once and kept in SRAM over all iterations
-        b_ptrs = Bias + offs_n
-        if EVEN_N:
-            bias = tl.load(b_ptrs).to(tl.float32)
-        else:
-            bias = tl.load(b_ptrs, mask=offs_n < seqlen_k, other=0.0).to(tl.float32)
-        bias = bias[None, :]
-    # k and v stay in SRAM throughout
-    # [2022-10-30] TD: Same bug as the fwd. In the case of EVEN_N=True and EVEN_M=False,
-    # if we just call tl.load(k_ptrs), we get the wrong output!
-    k_ptrs = K + ((offs_n * stride_kn)[:, None] + offs_d[None, :])
-    v_ptrs = V + ((offs_n * stride_vn)[:, None] + offs_d[None, :])
-    if EVEN_N & EVEN_M:
-        if EVEN_HEADDIM:
-            k = tl.load(k_ptrs)
-            v = tl.load(v_ptrs)
-        else:
-            k = tl.load(k_ptrs, mask=offs_d[None, :] < headdim, other=0.0)
-            v = tl.load(v_ptrs, mask=offs_d[None, :] < headdim, other=0.0)
-    else:
-        if EVEN_HEADDIM:
-            k = tl.load(k_ptrs, mask=offs_n[:, None] < seqlen_k, other=0.0)
-            v = tl.load(v_ptrs, mask=offs_n[:, None] < seqlen_k, other=0.0)
-        else:
-            k = tl.load(
-                k_ptrs, mask=(offs_n[:, None] < seqlen_k) & (offs_d[None, :] < headdim), other=0.0
-            )
-            v = tl.load(
-                v_ptrs, mask=(offs_n[:, None] < seqlen_k) & (offs_d[None, :] < headdim), other=0.0
-            )
-
-    # initialize dv and dk
-    if USE_DROPOUT:
-        # accumulate dKj and dVj in half precision to save SRAM/registers.
-        # will result in slightly increased numerical error.
-        dv = tl.zeros([BLOCK_N, BLOCK_HEADDIM], dtype=DV.dtype.element_ty)
-        dk = tl.zeros([BLOCK_N, BLOCK_HEADDIM], dtype=DK.dtype.element_ty)
-    else:
-        dv = tl.zeros([BLOCK_N, BLOCK_HEADDIM], dtype=tl.float32)
-        dk = tl.zeros([BLOCK_N, BLOCK_HEADDIM], dtype=tl.float32)
-
-    # loop over rows
-    # We need to make sure begin_m is a multiple of BLOCK_M (not BLOCK_N)
-    begin_m = 0 if not IS_CAUSAL else ((start_n * BLOCK_N) // BLOCK_M) * BLOCK_M
-    end_m = tl.cdiv(seqlen_q, BLOCK_M) * BLOCK_M
-    for start_m in range(begin_m, end_m, BLOCK_M):
-        start_m = tl.multiple_of(start_m, BLOCK_M)
-        offs_m_curr = start_m + tl.arange(0, BLOCK_M)
-        # load q, k, v, do on-chip
-        # Same bug as below. Otherwise gives wrong result for headdim=40, seqlen=(128, 117)
-
-        q_ptrs = Q + (offs_m_curr * stride_qm)[:, None] + offs_d[None, :]
-        if EVEN_M & EVEN_HEADDIM:
-            q = tl.load(q_ptrs)
-        else:
-            if EVEN_HEADDIM:
-                q = tl.load(q_ptrs, mask=offs_m_curr[:, None] < seqlen_q, other=0.0)
-            else:
-                q = tl.load(
-                    q_ptrs,
-                    mask=(offs_m_curr[:, None] < seqlen_q) & (offs_d[None, :] < headdim),
-                    other=0.0,
-                )
-
-        # recompute p = softmax(qk, dim=-1).T
-        s = softmax_scale * tl.dot(q, k, trans_b=True)
-        # Trying to combine the two masks seem to make the result wrong
-        if not EVEN_N:  # Need to mask out otherwise the softmax is wrong
-            s = tl.where(offs_n[None, :] < seqlen_k, s, float("-inf"))
-        if IS_CAUSAL:
-            s = tl.where(offs_m_curr[:, None] >= (offs_n[None, :]), s, float("-inf"))
-        if BIAS_TYPE != "none":
-            tl.debug_barrier()  # Race condition otherwise
-            if BIAS_TYPE == "vector":
-                pass  # already loaded before entering loop
-            elif BIAS_TYPE == "matrix":
-                b_ptrs = Bias + (offs_m_curr * stride_bm)[:, None] + offs_n[None, :]
-                if EVEN_M & EVEN_N:
-                    bias = tl.load(b_ptrs).to(tl.float32)
-                else:
-                    bias = tl.load(
-                        b_ptrs,
-                        mask=(offs_m_curr[:, None] < seqlen_q) & (offs_n[None, :] < seqlen_k),
-                        other=0.0,
-                    ).to(tl.float32)
-            s += bias
-        # There seems to be a race condition when headdim=48/96, and dq, dk, dv are wrong.
-        # Also wrong for headdim=64.
-        if not (EVEN_M & EVEN_HEADDIM):
-            tl.debug_barrier()
-        lse_i = tl.load(LSE + offs_m_curr)
-        p = tl.exp(s - lse_i[:, None])
-
-        if USE_DROPOUT:
-            # compute Zij (sort of, see below). has values:
-            #   1 / (1 - p) with probability 1 - p
-            #   0 with probability p
-
-            indices = (
-                dropout_rng_offset_hb.to(offs_m_curr.dtype)
-                + (offs_m_curr * seqlen_k)[:, None]
-                + offs_n[None, :]
-            )
-
-            # don't need to materialize Zij, just store drop bit into sign bit of Pij
-            # (trick stolen from CUDA implementation) to save SRAM/registers
-            dropout_mask = make_dropout_mask(dropout_p, rng_seed, indices)
-            p *= tl.where(dropout_mask, 1.0, -1.0)
-
-        # compute dv
-        # [2022-10-30] TD: A Triton bug: if EVEN_M=True and EVEN_HEADDIM=False, if we call
-        # do = tl.load(do_ptrs, mask=offs_d[None, :] < headdim, other=0.0), we get wrong outputs
-        # in the case of headdim=48/96, seqlen_q & seqlen_k >= 512. If headdim=40 or seqlen < 512,
-        # the output is correct.
-        do_ptrs = DO + (offs_m_curr * stride_dom)[:, None] + offs_d[None, :]
-        if EVEN_M & EVEN_HEADDIM:
-            do = tl.load(do_ptrs)
-        else:
-            # [2022-11-01] TD: Triton bug, there's a race condition if we just use m_mask and not d_mask.
-            do = tl.load(
-                do_ptrs,
-                mask=(offs_m_curr[:, None] < seqlen_q) & (offs_d[None, :] < headdim),
-                other=0.0,
-            )
-
-        if USE_DROPOUT:
-            p_dropped = tl.where(dropout_mask, p * (1.0 / (1.0 - dropout_p)), 0.0)
-            dv += tl.dot(p_dropped.to(do.dtype), do, trans_a=True).to(dv.dtype)
-        else:
-            dv += tl.dot(p.to(do.dtype), do, trans_a=True).to(dv.dtype)
-
-        # compute dp = dot(v, do)
-        # There seems to be a race condition when headdim=48/96, and dq, dk are wrong.
-        # Also wrong for headdim=128, seqlen=(108, 256)
-        if not (EVEN_M & EVEN_HEADDIM):
-            tl.debug_barrier()
-        dp = tl.dot(do, v, trans_b=True)
-        # we don't need to explicitly compute dPij from dPij_dropped, see below where
-        # we compute dSij for more details.
-
-        # There's a race condition for headdim=48
-        if not EVEN_HEADDIM:
-            tl.debug_barrier()
-        # compute ds = p * (dp - delta[:, None])
-        # Putting the subtraction after the dp matmul (instead of before) is slightly faster
-        Di = tl.load(D + offs_m_curr)
-
-        # Converting ds to q.dtype here reduces register pressure and makes it much faster
-        # for BLOCK_HEADDIM=128
-        if USE_DROPOUT:
-            # for computing dSij = tau * Pij * (dPij - Di)
-            # we have:
-            #   - Pij' which has negative values where Zij = 0, otherwise same as Pij
-            #   - dPij' = dPij_dropped
-            #
-            # IF KEEPING:
-            #   Pij = Pij'
-            #   dPij = dPij'
-            #
-            #   Pij * (dPij - Di) = Pij' * (dPij' - Di)
-            #                     = Pij' * ((dPij' * (1 / (1 - p))) - Di)
-            #
-            # IF DROPPING:
-            #   Pij = -Pij'
-            #   dPij = 0
-            #
-            #   Pij * (dPij - Di) = Pij * (0 - Di)
-            #                     = Pij * -Di
-            #                     = -Pij * Di
-            #                     = Pij' * Di
-            #   note earlier we flipped sign of p if element was to be dropped
-            ds = (
-                softmax_scale
-                * p
-                * tl.where(p > 0.0, (dp * (1.0 / (1.0 - dropout_p))) - Di[:, None], Di[:, None])
-            )
-            ds = ds.to(q.dtype)
-        else:
-            ds = (p * (dp - Di[:, None]) * softmax_scale).to(q.dtype)
-        # compute dk = dot(ds.T, q)
-        dk += tl.dot(ds, q, trans_a=True).to(dk.dtype)
-        # compute dq
-        dq_ptrs = DQ + (offs_m_curr * stride_dqm)[:, None] + offs_d[None, :]
-        dq = tl.dot(ds, k)
-        if EVEN_M & EVEN_HEADDIM:  # Race condition if we just do EVEN_M
-            tl.atomic_add(dq_ptrs, dq)
-        else:
-            if EVEN_HEADDIM:
-                tl.atomic_add(dq_ptrs, dq, mask=offs_m_curr[:, None] < seqlen_q)
-            else:
-                tl.atomic_add(
-                    dq_ptrs,
-                    dq,
-                    mask=(offs_m_curr[:, None] < seqlen_q) & (offs_d[None, :] < headdim),
-                )
-    # write-back
-    dv_ptrs = DV + ((offs_n * stride_dvn)[:, None] + offs_d[None, :])
-    dk_ptrs = DK + ((offs_n * stride_dkn)[:, None] + offs_d[None, :])
-    # [2022-11-01] TD: Same bug. In the case of EVEN_N=True and EVEN_M=False,
-    # if we just call tl.store(dv_ptrs), there's a race condition
-    if EVEN_N & EVEN_M:
-        if EVEN_HEADDIM:
-            tl.store(dv_ptrs, dv)
-            tl.store(dk_ptrs, dk)
-        else:
-            tl.store(dv_ptrs, dv, mask=offs_d[None, :] < headdim)
-            tl.store(dk_ptrs, dk, mask=offs_d[None, :] < headdim)
-    else:
-        if EVEN_HEADDIM:
-            tl.store(dv_ptrs, dv, mask=offs_n[:, None] < seqlen_k)
-            tl.store(dk_ptrs, dk, mask=offs_n[:, None] < seqlen_k)
-        else:
-            tl.store(dv_ptrs, dv, mask=(offs_n[:, None] < seqlen_k) & (offs_d[None, :] < headdim))
-            tl.store(dk_ptrs, dk, mask=(offs_n[:, None] < seqlen_k) & (offs_d[None, :] < headdim))
 
 
 def init_to_zero(name):
@@ -974,8 +710,6 @@ def _flash_attn_forward(q, k, v, bias=None, causal=False, dropout_p=0.0, softmax
         d,
         seqlen_q // 32,
         seqlen_k // 32,  # key for triton cache (limit number of compilations)
-        # Can't use kwargs here because triton autotune expects key to be args, not kwargs
-        # IS_CAUSAL=causal, BLOCK_HEADDIM=d,
         dropout_p > 0.0,  # USE_DROPOUT
         bias_type,
         causal,
@@ -1109,14 +843,9 @@ def _flash_attn_backward(
         seqlen_q // 32,
         seqlen_k // 32,  # key for triton cache (limit number of compilations)
         dropout_p > 0.0,  # USE_DROPOUT
-        # Can't use kwargs here because triton autotune expects key to be args, not kwargs
-        # IS_CAUSAL=causal, BLOCK_HEADDIM=d,
         bias_type,
         causal,
         BLOCK_HEADDIM,
-        # BLOCK_M=BLOCK_M, BLOCK_N=BLOCK_N,
-        # num_warps=num_warps,
-        # num_stages=1,
     )
     dq.copy_(dq_accum)
 
